e5ca1c2e3c07ce6a3cd16ecd8ff23af63410958d
DarmaniLink <ben.hirasuna@gmail.com>
2018-12-23 20:53:29 -0800

    first commit

---
+++ brainfuck-cmd-interpreter.cpp
@@ -0,0 +1,59 @@
+
+#include <iostream>
+#include <vector>
+#define pc(x) putchar(x)
+#define gc() getchar()
+
+ int main(int argc, char** argv){
+    char* data = (char*) calloc(30000, sizeof(char));
+    char* input = argc > 0 ? argv[1] : (char*)0x0;
+    char* data_ptr = data;
+    if(input == 0x0){
+        std::cout << "No string has been entered: exiting. . .";
+        return 0;
+    }
+        std::vector<char*>jump_loc;
+        for(char* instr_ptr = input; ; instr_ptr++){
+            switch(*instr_ptr){
+                case '>':
+                    data_ptr++;  break;  
+                case '<':
+                    data_ptr--;  break;
+                case '+':           
+                   ++*data_ptr; break;
+                case '-':
+                    --*data_ptr; break;
+                case '.':
+                    pc(*data_ptr); break;
+                case ',':
+                    *data_ptr=gc(); break;
+
+                case ']':
+                    if(*data_ptr){
+                        instr_ptr = jump_loc.back();
+                        instr_ptr--; 
+                    }
+                    jump_loc.pop_back();
+                    break;
+                case '[':
+                    if(*data_ptr)
+                        jump_loc.push_back(instr_ptr); 
+                    else
+                        for(;*instr_ptr != ']'; instr_ptr++);
+                    
+                    break;
+
+                default:  
+                    char* inst_peek = instr_ptr;
+                    if(*inst_peek =='E')
+                        if(*(++inst_peek) =='N')
+                            if(*(++inst_peek) =='D'){
+                                std::cout << std::endl << "end sequence found: terminating. . . ";
+                                return 0;
+                            }
+                    if(*inst_peek == '\0')
+                        return 0;       
+            }
+        }
+     return 0;
+ }
